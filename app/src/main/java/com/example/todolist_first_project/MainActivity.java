05ea598d5ec4b7beb28b161898069b86556e29f9
laracoo <larianaclap@gmail.com>
2020-02-01 22:36:44 +0700

    This project will be a To Do List with a database and direct connection via email.

---
+++ app/src/main/java/com/example/todolist_first_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.todolist_first_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// This project will be a To Do List with a database and direct connection via email.
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
